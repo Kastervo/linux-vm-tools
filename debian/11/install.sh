eab3d51ca631948c220697cb4fede74dc10a51bd
kastervoadmin <42879399+kastervoadmin@users.noreply.github.com>
2021-12-18 05:23:11 +0200

    Update install.sh

--- debian/11/install.sh
+++ debian/11/install.sh
@@ -54,15 +54,15 @@ sudo checkinstall --pkgname=xorgxrdp --pkgversion=1:$pkgver --pkgrelease=1 --def
 
 # Configure the installed XRDP ini files.
 # use vsock transport.
-sed -i_orig -e 's/use_vsock=true/use_vsock=false/g' /etc/xrdp/xrdp.ini
+#sed -i_orig -e 's/use_vsock=true/use_vsock=false/g' /etc/xrdp/xrdp.ini
 # change the port
-sed -i_orig -e 's@port=3389@port=tcp://:3389@g' /etc/xrdp/xrdp.ini
+#sed -i_orig -e 's@port=3389@port=tcp://:3389@g' /etc/xrdp/xrdp.ini
 # use rdp security.
-sed -i_orig -e 's/security_layer=negotiate/security_layer=rdp/g' /etc/xrdp/xrdp.ini
+#sed -i_orig -e 's/security_layer=negotiate/security_layer=rdp/g' /etc/xrdp/xrdp.ini
 # remove encryption validation.
-sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
+#sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
 # disable bitmap compression since its local its much faster
-sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
+#sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
 
 # Add script to setup the debian session properly
 if [ ! -e /etc/xrdp/startdebian.sh ]; then
@@ -76,25 +76,25 @@ chmod a+x /etc/xrdp/startdebian.sh
 fi
 
 # use the script to setup the debian session
-sed -i_orig -e 's/startwm/startdebian/g' /etc/xrdp/sesman.ini
+#sed -i_orig -e 's/startwm/startdebian/g' /etc/xrdp/sesman.ini
 
 # rename the redirected drives to 'shared-drives'
-sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc/xrdp/sesman.ini
+#sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc/xrdp/sesman.ini
 
 # Changed the allowed_users
-sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
+#sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
 
 # Blacklist the vmw module
-if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ]; then
-cat >> /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf <<EOF
-blacklist vmw_vsock_vmci_transport
-EOF
-fi
+#if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ]; then
+#cat >> /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf <<EOF
+#blacklist vmw_vsock_vmci_transport
+#EOF
+#fi
 
 #Ensure hv_sock gets loaded
-if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
-echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
-fi
+#if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
+#echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
+#fi
 
 # Configure the policy xrdp session
 cat > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
