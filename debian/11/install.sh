c711070b35fec068a2f29a117320bc7f5b9392c8
kastervoadmin <42879399+kastervoadmin@users.noreply.github.com>
2021-12-18 18:26:14 +0200

    Update install.sh

--- debian/11/install.sh
+++ debian/11/install.sh
@@ -16,8 +16,8 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-sudo apt update && apt upgrade -y
-sudo apt install libconfig-dev build-essential dpkg-dev libpulse-dev libx11-dev libxfixes-dev libssl-dev libpam0g-dev libtool libjpeg-dev flex bison gettext autoconf libxml-parser-perl libfuse-dev xsltproc libxrandr-dev python3-libxml2 nasm fuse pkg-config git intltool checkinstall xserver-xorg-dev -y
+sudo apt update && sudo apt upgrade -y
+#sudo apt install libconfig-dev build-essential dpkg-dev libpulse-dev libx11-dev libxfixes-dev libssl-dev libpam0g-dev libtool libjpeg-dev flex bison gettext autoconf libxml-parser-perl libfuse-dev xsltproc libxrandr-dev python3-libxml2 nasm fuse pkg-config git intltool checkinstall xserver-xorg-dev -y
 
 if [ -f /var/run/reboot-required ]; then
     echo "A reboot is required in order to proceed with the install." >&2
@@ -25,44 +25,52 @@ if [ -f /var/run/reboot-required ]; then
     exit 1
 fi
 
+#
+#
+###############################################################################
+
 ###############################################################################
 # XRDP
 #
 
-sudo mkdir /usr/local/lib/xrdp/
+sudo apt install xrdp -y
 
-cd /tmp
+#sudo apt install libx11-dev libxfixes-dev libssl-dev libpam0g-dev libtool libjpeg-dev flex bison gettext autoconf libxml-parser-perl libfuse-dev xsltproc libxrandr-dev python3-libxml2 nasm fuse pkg-config git intltool checkinstall -y
 
-git clone https://github.com/neutrinolabs/xorgxrdp.git
-git clone https://github.com/neutrinolabs/xrdp.git
+#sudo mkdir /usr/local/lib/xrdp/
 
-cd /tmp/xrdp
+#cd /tmp
 
-sudo ./bootstrap
-sudo ./configure
-sudo make
+#git clone https://github.com/neutrinolabs/xorgxrdp.git
+#git clone https://github.com/neutrinolabs/xrdp.git
 
-sudo checkinstall --pkgname=xrdp --pkgversion=$pkgver --pkgrelease=1 --default
+#cd /tmp/xrdp
 
-cd /tmp/xorgxrdp
+#sudo ./bootstrap
+#sudo ./configure
+#sudo make
 
-sudo ./bootstrap
-sudo ./configure
-sudo make
+#sudo checkinstall --pkgname=xrdp --pkgversion=$pkgver --pkgrelease=1 --default
 
-sudo checkinstall --pkgname=xorgxrdp --pkgversion=1:$pkgver --pkgrelease=1 --default
+#cd /tmp/xorgxrdp
+
+#sudo ./bootstrap
+#sudo ./configure
+#sudo make
+
+#sudo checkinstall --pkgname=xorgxrdp --pkgversion=1:$pkgver --pkgrelease=1 --default
 
 # Configure the installed XRDP ini files.
 # use vsock transport.
-#sed -i_orig -e 's/use_vsock=true/use_vsock=false/g' /etc/xrdp/xrdp.ini
+sed -i_orig -e 's/use_vsock=true/use_vsock=false/g' /etc/xrdp/xrdp.ini
 # change the port
-#sed -i_orig -e 's@port=3389@port=tcp://:3389@g' /etc/xrdp/xrdp.ini
+sed -i_orig -e 's@port=3389@port=vsock://-1:3389@g' /etc/xrdp/xrdp.ini
 # use rdp security.
-#sed -i_orig -e 's/security_layer=negotiate/security_layer=rdp/g' /etc/xrdp/xrdp.ini
+sed -i_orig -e 's/security_layer=negotiate/security_layer=rdp/g' /etc/xrdp/xrdp.ini
 # remove encryption validation.
-#sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
+sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
 # disable bitmap compression since its local its much faster
-#sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
+sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
 
 # Add script to setup the debian session properly
 if [ ! -e /etc/xrdp/startdebian.sh ]; then
@@ -76,25 +84,25 @@ chmod a+x /etc/xrdp/startdebian.sh
 fi
 
 # use the script to setup the debian session
-#sed -i_orig -e 's/startwm/startdebian/g' /etc/xrdp/sesman.ini
+sed -i_orig -e 's/startwm/startdebian/g' /etc/xrdp/sesman.ini
 
 # rename the redirected drives to 'shared-drives'
-#sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc/xrdp/sesman.ini
+sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc/xrdp/sesman.ini
 
 # Changed the allowed_users
-#sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
+sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
 
 # Blacklist the vmw module
-#if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ]; then
-#cat >> /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf <<EOF
-#blacklist vmw_vsock_vmci_transport
-#EOF
-#fi
+if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ]; then
+cat >> /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf <<EOF
+blacklist vmw_vsock_vmci_transport
+EOF
+fi
 
 #Ensure hv_sock gets loaded
-#if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
-#echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
-#fi
+if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
+echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
+fi
 
 # Configure the policy xrdp session
 cat > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
@@ -116,21 +124,33 @@ systemctl start xrdp
 # End XRDP
 ###############################################################################
 
+###############################################################################
 # Pulseaudio
+#
 
 pulsever=$(pulseaudio --version | awk '{print $2}')
+
+sudo apt install git libconfig-dev libpulse-dev autoconf m4 intltool build-essential dpkg-dev libtool libsndfile-dev libcap-dev -y libjson-c-dev
+
 sudo apt build-dep pulseaudio -y
+
 cd /tmp
 sudo apt source pulseaudio=$pulsever
 cd /tmp/pulseaudio-$pulsever
 sudo ./configure
+
 git clone https://github.com/neutrinolabs/pulseaudio-module-xrdp.git
 cd pulseaudio-module-xrdp
 sudo ./bootstrap
 sudo ./configure PULSE_DIR="/tmp/pulseaudio-$pulsever"
 sudo make
+
 cd /tmp/pulseaudio-$pulsever/pulseaudio-module-xrdp/src/.libs
 sudo install -t "/var/lib/xrdp-pulseaudio-installer" -D -m 644 *.so
 sudo install -t "/usr/lib/pulse-$pulsever/modules" -D -m 644 *.so
+
+#
+# End Pulseaudio
+###############################################################################
 
 echo "Reboot your machine to begin using XRDP."
